c9ae8d012ecade8bbde6057f8c5fe15c962c7942
Mike J McGuire <mike@mcguire.mx>
2024-07-05 14:58:39 +1000

    Update sslpsk.py

--- sslpsk/sslpsk.py
+++ sslpsk/sslpsk.py
@@ -12,6 +12,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License
 
+# Updated by doronz88, but not yet updated into original repository from drbild
+
 from __future__ import absolute_import
 
 import _ssl
